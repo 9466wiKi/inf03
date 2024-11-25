8d9586e671a03280916d5ffa2c3766d0b2bbc779
Wiktoria <wikioki@vp.pl>
2024-11-25 14:14:30 +0100

    EE.09-03-23.01-SG

---
+++ EE.09-03-23.01-SG/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styl5.css">
+	<title>sekretariat</title>
+</head>
+<body>
+    <section id="block-left">
+        <h1>Akta Prawnicze</h1>
+        <hr>
+        <a href="cv.txt">Pobierz</a>
+        <h1>Liczba zatrudnionych pracowników</h1>
+        <p></p>
+    </section>
+    <section id="block-right">
+    </section>
+    <section id="footer">
+        Autorem aplikacji jest:5949838989
+        <ul>
+            <li>Skontaktuj się</li>
+            <li>Poznaj naszą firmę</li>
+        </ul>
+    </section>
+</body>
+</html>
